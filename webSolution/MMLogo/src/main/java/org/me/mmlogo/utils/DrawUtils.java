7b8ff6844bad279b87c87c95086d90fc9b40fcbe
Ivelin1936 <ivelin_1936@abv.bg>
2019-05-08 16:53:14 +0300

    # MMLogo web * remove utils package

--- webSolution/MMLogo/src/main/java/org/me/mmlogo/utils/DrawUtils.java
+++
@@ -1,8 +0,0 @@
-package org.me.mmlogo.utils;
-
-public class DrawUtils {
-
-    public static String drawSequence(String element, int count) {
-        return new String(new char[count]).replace("\0", element);
-    }
-}
